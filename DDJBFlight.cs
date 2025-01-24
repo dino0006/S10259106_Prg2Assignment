3b889e11e2ceb0128ca3e6ca931af9a119127797
guangcheng2 <guangcheng0129@gmail.com>
2025-01-24 23:52:53 +0800

    added all the flights classes terminal and airline

---
+++ DDJBFlight.cs
@@ -0,0 +1,16 @@
+﻿public class DDJBFlight : Flight
+{
+    private double requestFee;
+    public double RequestFee
+    {
+        get { return requestFee; }
+        set { requestFee = value; }
+    }
+    public override double CalculateFees()
+    {
+        return RequestFee;
+    }
+    public override string ToString
+    {
+        return 
+    }
