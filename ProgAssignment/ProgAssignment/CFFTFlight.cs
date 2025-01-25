d3a2154fbdce5a53ef1cd82338fcf0ceaef6def0
guangcheng2 <guangcheng0129@gmail.com>
2025-01-25 14:18:15 +0800

    completed all the flights.cs

---
+++ ProgAssignment/ProgAssignment/CFFTFlight.cs
@@ -0,0 +1,16 @@
+﻿public class CFFTFlight : Flight
+{
+    private double requestFee;
+    public double requestFee
+    {
+        get { return requestFee; }
+        set { requestFee = value; }
+    }
+    public override double CalculateFees()
+    {
+        return requestFee
+    }
+    public override string ToString()
+    {
+        return
+    }
